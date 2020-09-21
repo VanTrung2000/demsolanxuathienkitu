324fe230b43c61287f8e0a86b652ec680ad68e23
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-21 19:48:53 +0700

    Add project files.

---
+++ demsolanxuathienkitu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace demsolanxuathienkitu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = { "anh", "trung", "dep", "trai", "trung" };
+            Console.WriteLine("Nhap ky tu co trong mang : ");
+            string x = Console.ReadLine();
+            Console.WriteLine("So lan xuat hien cua ky tu {0} là  ", x);
+            count(s, x);
+            static void count(string[] z, string n)
+            {
+                int dem = 0;
+                for (int i = 0; i < z.Length; i++)
+                {
+                    if (z[i] == n)
+                        dem++;
+
+                }
+                Console.WriteLine(dem);
+            }
+        }
+    }
+}
